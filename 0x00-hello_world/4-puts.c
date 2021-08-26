f80ce0c13b5cdc351ab6f2a5db43fbe3b1076289
martinDdev <mwendiiimwenda@gmail.com>
2021-08-26 19:56:12 +0000

    printing with puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-  puts("Programming is like building a multilingual puzzle");
-  getchar(); 
+  puts("Programming is like building a multilingual puzzle\n");
+  getchar();
   return 0;
 }
