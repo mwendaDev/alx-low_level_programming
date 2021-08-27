a2dafab7507798a6980b15a3ca92f543f3a13ca2
martinDdev <mwendiiimwenda@gmail.com>
2021-08-27 21:36:21 +0000

    task4putchar

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,11 +7,14 @@ int main(void)
 {
 char ch;
 ch = 'a';
-while (ch <= 'z' && (ch != 'q' || 'e'))
+if (ch != 'q' || 'e')
+{
+while (ch <= 'z')
 {
 putchar(ch);
 ch++;
 }
 putchar('\n');
 return (0);
+}
 }
