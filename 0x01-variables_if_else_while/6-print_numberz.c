39a531dc6011e646b3bc849f3bbb5bfe7f7108ad
martinDdev <mwendiiimwenda@gmail.com>
2021-08-28 22:47:44 +0000

    putchar() and single digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main -entry point
+*Return: 0 if executed
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+if ((i == 0) || (i'%10' + '0'))
+{
+putchar(('0') || (i'%10' + '0'));
+}
+}
+putchar('\n');
+return (0);
+}
