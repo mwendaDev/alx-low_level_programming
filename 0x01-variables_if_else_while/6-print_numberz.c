1a94095452b39923a88a43ba08758e82dfa140d9
martinDdev <mwendiiimwenda@gmail.com>
2021-08-28 23:04:10 +0000

    putchar() and single digits

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,11 +5,13 @@
 */
 int main(void)
 {
-for (int i = 0; i < 10; i++)
+int i;
+for (i = 0; i < 10; i++)
 {
 if ((i == 0) || (i % 10 + '0'))
 {
 putchar(('0') || (i % 10 + '0'));
+print(i);
 }
 }
 putchar('\n');
