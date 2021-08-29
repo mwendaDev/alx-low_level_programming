b246bce8a276392d90994bc49cdce0ea4bc817c8
martinDdev <mwendiiimwenda@gmail.com>
2021-08-29 02:19:42 +0000

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,16 +6,16 @@
 int main(void)
 {
 int i;
-char ch;
 for (i = 0; i < 10; i++)
 {
 if ((i == 0) || (i % 10 + '0'))
 putchar(i % 10 + '0');
+}
+char ch;
 for (ch = 'a'; ch <= 'f'; ch++)
 {
 putchar(ch);
 }
 putchar('\n');
 return (0);
-}
 }
