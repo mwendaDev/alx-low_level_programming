6313ca7a95460b7b386b8f6a3d78d35954ea13bf
martinDdev <mwendiiimwenda@gmail.com>
2021-08-29 20:31:21 +0000

    stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 1 if not executed successfully
+*/
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
