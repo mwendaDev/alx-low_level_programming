546bc6a1baf795091f1dfa190f67eaee308a08b7
martinDdev <mwendiiimwenda@gmail.com>
2021-08-30 20:49:19 +0000

    3digit combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+*main - entry point
+*Return: 0 if executed successfully
+*/
+int main(void)
+{
+int i;
+int j;
+int k;
+for (i = 0; i < 8; i++)
+for (j = 1; j < 9; j++)
+for (k = 2; k < 10; k++)
+{
+{
+{
+if ((i == j) || (j == k) || (i == k)
+|| (j == k) || (j == i) || (i > j) || (j > K))
+continue;
+putchar(i % 10 + '0');
+putchar(j % 10 + '0');
+putchar(k % 10 + '0');
+if ((i == 7) && (j == 8) && (k == 9))
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
