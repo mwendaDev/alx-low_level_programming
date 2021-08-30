f6b8536da91233b965aaac7e446ce8fcb2aa9c67
martinDdev <mwendiiimwenda@gmail.com>
2021-08-30 09:58:57 +0000

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+*main - entry point
+*Return: 0 if executed successfullyi
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i % 10 + '0');
+if (i == 9)
+continue;
+putchar(",");
+putchar(" ");
+}
+putchar('\n');
+return (0);
+}
