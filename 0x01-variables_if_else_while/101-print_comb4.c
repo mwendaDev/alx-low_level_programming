f17b736bbb0c03a0bd1092be00eb3e0288ff9fcc
martinDdev <mwendiiimwenda@gmail.com>
2021-08-30 21:54:17 +0000

    3digit combo

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,8 +15,8 @@ for (k = 2; k < 10; k++)
 {
 {
 if ((i == k) || (i == j) || (j == k))
-if ((i > j) || (i > k) || (j > k))
 continue;
+if ((i > j) || (i > k) || (j > k))
 continue;
 putchar(i % 10 + '0');
 putchar(j % 10 + '0');
