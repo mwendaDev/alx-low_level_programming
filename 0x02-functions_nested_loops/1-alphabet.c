980c4a7e51c11df4989f659f2204a22b0712a202
martinDdev <mwendiiimwenda@gmail.com>
2021-09-01 21:12:04 +0000

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
 *print_alphabet - function to print alphabet
-*Return: 0 if executed successfully
+*Return: if executed successfully
 */
-void print_alphabet()
+void print_alphabet(void)
 {
 char c[26] = "a-z";
 int i;
@@ -12,6 +12,5 @@ for (i = 0; i <= 26; i++)
 _putchar(c[i]);
 }
 _putchar('\n');
-return (0);
 }
 
