ca8688592f57e95c85722882f19e13c3382d39d7
martinDdev <mwendiiimwenda@gmail.com>
2021-09-11 20:24:09 +0000

    _putchar prints alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_isalpha - function to check for uppercase character
+*@c: character to print
+*Return: on success 1
+*/
+int _isalpha(int c)
+{
+int i;
+int j;
+for (i = 97; i <= 122; i++)
+for (j = 65; j <= 90 ; j++)
+{
+{
+if ((c == 99) || (C == 67) || (i == c) || (j == C))
+return (1);
+}
+}
+return (0);
+}
