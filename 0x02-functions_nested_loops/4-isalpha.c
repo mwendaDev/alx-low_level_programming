abf380d50559b004c787d0b2bfd4338dccc3057e
martinDdev <mwendiiimwenda@gmail.com>
2021-09-11 20:32:19 +0000

    _putchar prints alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,16 +6,14 @@
 */
 int _isalpha(int c)
 {
-int C;
 int i;
 int j;
 for (i = 97; i <= 122; i++)
 for (j = 65; j <= 90 ; j++)
 {
 {
-if ((c == 99) || (C == 67) || (i == c) || (j == C))
+if ((c == 99) || (c == 67) || (i == c))
 return (1);
 }
 }
 return (0);
-}
