1783a7432709435a89c7ff9beae737aecd44969d
martinDdev <mwendiiimwenda@gmail.com>
2021-09-17 12:30:11 +0000

    Print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*print_sign - function to print a sign
+*@n: reference integer
+*Return: on success 1
+*/
+int print_sign(int n)
+{
+int i = 43;
+int j = 45;
+int k = 48;
+if (n > 0)
+{
+_putchar(i);
+}
+else if (n == 0)
+{
+_putchar(k);
+return (0);
+}
+else
+{
+_putchar(45);
+return (-1);
+}
+}
