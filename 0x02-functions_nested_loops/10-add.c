5bba3ccc53650c61b33e8c61e3b9e193124d803a
martinDdev <mwendiiimwenda@gmail.com>
2021-09-18 22:30:02 +0000

    Addition

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,10 +2,12 @@
 /**
 *add - addition function
 *Return: on success sum
-*@num1,num2: numbers to add 
+*
 */
-int add(int num1, int num2)
+int add(int, int)
 {
+int num1;
+int num2;
 int sum;
 sum = (num1 + num2);
 return (sum);
