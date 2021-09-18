6b212f4047f25fe8284f990ea5ba1d6cc6af9671
martinDdev <mwendiiimwenda@gmail.com>
2021-09-18 22:14:19 +0000

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*add - addition function
+*Return: on success sum
+*@int: numbers to add
+*/
+int add(int, int)
+{
+num1;
+num2;
+int sum;
+sum = (num1 + num2);
+return (sum);
+}
