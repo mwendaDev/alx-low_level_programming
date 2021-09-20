b4818020bfa54df408afa23d317727409370f61c
martinDdev <mwendiiimwenda@gmail.com>
2021-09-20 12:39:05 +0000

    Largest of three

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if ((a > b) && (a > c) && (a != b || c))
+if ((a >= b) && (b > c))
 {
 largest = a;
 }
-else if ((b > a) && (b > c) && (b != a || c))
+else if ((b >= a) && (a > c))
 {
 largest = b;
 }
