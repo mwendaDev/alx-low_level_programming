c75140c0c2098af91501de8f7a6fc7cd54d27f43
martinDdev <mwendiiimwenda@gmail.com>
2021-09-27 18:54:52 +0000

    printing a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+/**
+*print_line - func to ptint line
+*@n: number of characters to print
+*/
+void print_line(int n)
+{
+int i;
+int k = 95;
+for (i = 1; i <= n; i++)
+{
+if ((n != 0) || (n > 0))
+_putchar(k);
+}
+return (0);
+}
