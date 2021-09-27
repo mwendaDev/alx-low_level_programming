b850d8ac4040e24e71eb254cc3a22671dabe6faa
martinDdev <mwendiiimwenda@gmail.com>
2021-09-27 11:15:31 +0000

    is it uppercase?

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,12 +7,16 @@
 int _isupper(int c)
 {
 int i;
+int j;
+for (j = 32; j <= 126; j++)
 for (i = 65; i <= 90; i++)
 {
-if (c = i)
+{
+if (c == i)
 return (1);
 else
 return (0);
+}
 }
 return (0);
 }
