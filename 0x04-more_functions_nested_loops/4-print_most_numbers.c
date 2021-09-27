0a507385ce30d1f1744e1f8c7cd9984d6c48410f
martinDdev <mwendiiimwenda@gmail.com>
2021-09-27 12:23:30 +0000

    print 0-9 excluding 2&4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_most_numbers - func to print 0-9 excluding 2&4
+*/
+void print_most_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+if ((i == 50) || (i == 52))
+continue;
+_putchar(i);
+}
+_putchar('\n');
+}
